c520a01fdacc44b33c105daf7f748ea3697cb495
matthewross <mattross@csumb.edu>
2024-07-16 18:08:19 -0700

    added MeleeAttack.java and RangeAttack.java

---
+++ src/main/java/MeleeAttack.java
@@ -0,0 +1,12 @@
+public class MeleeAttack implements Attack{
+    Monster attacker;
+
+    public MeleeAttack(Monster attacker) {
+        this.attacker = attacker;
+    }
+    public Integer attack(Monster monster){
+        String message= attacker+" uses a melee attack on "+monster;
+        System.out.println(message);
+        return null;
+    }
+}
---
+++ src/main/java/RangeAttack.java
@@ -0,0 +1,12 @@
+public class RangeAttack implements Attack{
+    Monster attacker;
+
+    public RangeAttack(Monster attacker) {
+        this.attacker = attacker;
+    }
+    public Integer attack(Monster monster){
+        String message= attacker+" uses a range attack on "+monster;
+        System.out.println(message);
+        return null;
+    }
+}
